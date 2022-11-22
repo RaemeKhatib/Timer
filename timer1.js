6c69863766bafb934888cc393c70accabfa09726
Raeme Khatib <raeme.khatib@telus.com>
2022-11-22 15:43:15 -0700

    made sure that we can put seconds and it will convert to milliseconds

--- timer1.js
+++ timer1.js
@@ -2,7 +2,8 @@
 
 
 for (const step of process.argv) {
-  if (step >= 0) {
-    setTimeout(() => process.stdout.write('\x07'), step);
-  }
-}
+  if (step >= 0){
+    let delay = step * 1000
+    setTimeout(() => process.stdout.write('\x07'), delay);
+  }  
+};
